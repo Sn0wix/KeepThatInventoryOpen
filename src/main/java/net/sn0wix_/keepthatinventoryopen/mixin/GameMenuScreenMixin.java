5269106073a4eb56a986c2a12260f190c8f0d6c1
Sn0wix <sn0wix@proton.me>
2026-03-02 21:38:50 +0100

    Revert "1.21.8 compatibility"
    This reverts commit 51d2c00c42113e955092deb328f1223f37ac0b38.

--- src/main/java/net/sn0wix_/keepthatinventoryopen/mixin/GameMenuScreenMixin.java
+++ src/main/java/net/sn0wix_/keepthatinventoryopen/mixin/GameMenuScreenMixin.java
@@ -5,14 +5,12 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ConfirmScreen;
 import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.input.MouseInput;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import net.sn0wix_.keepthatinventoryopen.config.Settings;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import org.jspecify.annotations.NonNull;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -20,21 +18,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameMenuScreen.class)
 public class GameMenuScreenMixin {
-    @Shadow
-    private @Nullable ButtonWidget exitButton;
-    @Unique
-    private static boolean shouldContinue = true;
-
     //this.exitButton = adder.add(ButtonWidget.builder(text, button -> { <--HERE
     @Inject(method = "method_19836", at = @At("HEAD"), cancellable = true)
     private void injectOnDisconnect(ButtonWidget button, CallbackInfo ci) {
         try {
-            if (shouldContinue && Settings.enabled.getValue()) {
+            if (Settings.enabled.getValue()) {
                 MinecraftClient client = MinecraftClient.getInstance();
+
 
                 if (Settings.displayWarning.getValue() &&
                         !(client.player.playerScreenHandler.getCraftingInput().getHeldStacks().stream().allMatch(ItemStack::isEmpty) &&
-                                client.player.playerScreenHandler.getCursorStack().isEmpty())) {
+                        client.player.playerScreenHandler.getCursorStack().isEmpty())) {
 
                     ConfirmScreen screen = getConfirmScreen(client);
                     client.setScreen(screen);
@@ -47,12 +41,10 @@ public class GameMenuScreenMixin {
     }
 
     @Unique
-    private @NotNull ConfirmScreen getConfirmScreen(MinecraftClient client) {
+    private @NonNull ConfirmScreen getConfirmScreen(MinecraftClient client) {
         BooleanConsumer callback = b -> {
             if (b) {
-                shouldContinue = false;
-                exitButton.onPress(new MouseInput(0, 0));
-                shouldContinue = true;
+                client.getAbuseReportContext().tryShowDraftScreen(client, ((GameMenuScreen) (Object) this), () -> client.disconnect(ClientWorld.QUITTING_MULTIPLAYER_TEXT), true);
             } else {
                 client.setScreen(null);
             }

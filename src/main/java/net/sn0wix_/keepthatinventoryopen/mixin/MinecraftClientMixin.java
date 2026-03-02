d2d6d454660a98ddf14f3a86c5fce4be1d0dde24
Sn0wix <sn0wix@proton.me>
2026-03-02 21:31:11 +0100

    port

--- src/main/java/net/sn0wix_/keepthatinventoryopen/mixin/MinecraftClientMixin.java
+++ src/main/java/net/sn0wix_/keepthatinventoryopen/mixin/MinecraftClientMixin.java
@@ -1,8 +1,8 @@
 package net.sn0wix_.keepthatinventoryopen.mixin;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.network.packet.c2s.play.CloseHandledScreenC2SPacket;
-import net.minecraft.text.Text;
 import net.sn0wix_.keepthatinventoryopen.KeepThatInventoryOpen;
 import net.sn0wix_.keepthatinventoryopen.config.Settings;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,9 +12,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
-    @Inject(method = "disconnect(Lnet/minecraft/text/Text;)V", at = @At("HEAD"))
-    private void injectDisconnect(Text reasonText, CallbackInfo ci){
-        if (Settings.enabled.getValue() && Settings.onDisconnect.getValue()) {
+    @Inject(method = "disconnect(Lnet/minecraft/client/gui/screen/Screen;Z)V", at = @At("HEAD"))
+    private void injectDisconnect(Screen disconnectionScreen, boolean transferring, CallbackInfo ci){
+        if (Settings.enabled.getValue() && Settings.onDisconnect.getValue() && MinecraftClient.getInstance().getNetworkHandler() != null) {
             MinecraftClient.getInstance().getNetworkHandler().sendPacket(new CloseHandledScreenC2SPacket(0));
             KeepThatInventoryOpen.LOGGER.info("Stimulated close inventory packet upon disconnecting");
         }
